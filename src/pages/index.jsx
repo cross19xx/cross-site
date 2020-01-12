17c1276464d0ce5629cd862741fdd33906e9f86e
Kenneth Kwakye-Gyamfi <kenneth.cross95@gmail.com>
2020-01-12 19:54:20 +0000

    Setup the CSS and font styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import '../css/home.css';
+
+import Layout from '../components/Layout';
+
 export default function Homepage() {
-    return <h1>Welcome to the official website from Kenneth Kwakye-Gyamfi</h1>;
+    return (
+        <Layout className="home" canonicalUrl="">
+            <React.Fragment>
+                <h1>Welcome to the official website from Kenneth Kwakye-Gyamfi</h1>
+            </React.Fragment>
+        </Layout>
+    );
 }
