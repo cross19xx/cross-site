43ffca03e3040314cb0b65acf4a311202d8fa83b
Kenneth Kwakye-Gyamfi <kenneth.cross95@gmail.com>
2020-01-30 14:19:45 +0000

    Removed call to event listener

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,56 +3,43 @@ import React from 'react';
 // Change this if you want to update the form name in Netlify
 const NETLIFY_FORM_NAME = 'contact';
 
-export default class ContactForm extends React.Component {
-    state = {};
+export default function ContactForm() {
+    return (
+        <form
+            method="post"
+            data-netlify="true"
+            name={NETLIFY_FORM_NAME}
+            className="contact-form grid"
+            data-netlify-honeypot="bot-field">
+            <input type="hidden" name="form-name" value={NETLIFY_FORM_NAME} />
 
-    submitForm = async event => {
-        event.preventDefault();
-    };
+            <div className="contact-form__container">
+                <label htmlFor="name" className="contact-form__label">
+                    Name
+                </label>
 
-    render() {
-        return (
-            <form
-                method="post"
-                data-netlify="true"
-                name={NETLIFY_FORM_NAME}
-                onSubmit={this.submitForm}
-                className="contact-form grid"
-                data-netlify-honeypot="bot-field">
-                <input type="hidden" name="form-name" value={NETLIFY_FORM_NAME} />
+                <input type="text" id="name" name="name" className="contact-form__input" />
+            </div>
 
-                <div className="contact-form__container">
-                    <label htmlFor="name" className="contact-form__label">
-                        Name
-                    </label>
+            <div className="contact-form__container">
+                <label htmlFor="email" className="contact-form__label">
+                    E-mail address
+                </label>
 
-                    <input type="text" id="name" name="name" className="contact-form__input" />
-                </div>
+                <input type="email" id="email" name="email" className="contact-form__input" />
+            </div>
 
-                <div className="contact-form__container">
-                    <label htmlFor="email" className="contact-form__label">
-                        E-mail address
-                    </label>
+            <div className="contact-form__container contact-form__container--textarea">
+                <label htmlFor="description" className="contact-form__label">
+                    Details
+                </label>
 
-                    <input type="email" id="email" name="email" className="contact-form__input" />
-                </div>
+                <textarea id="description" name="description" className="contact-form__textarea" />
+            </div>
 
-                <div className="contact-form__container contact-form__container--textarea">
-                    <label htmlFor="description" className="contact-form__label">
-                        Details
-                    </label>
-
-                    <textarea
-                        id="description"
-                        name="description"
-                        className="contact-form__textarea"
-                    />
-                </div>
-
-                <button type="submit" className="contact-form__submit">
-                    Send mail
-                </button>
-            </form>
-        );
-    }
+            <button type="submit" className="contact-form__submit">
+                Send mail
+            </button>
+        </form>
+    );
 }
